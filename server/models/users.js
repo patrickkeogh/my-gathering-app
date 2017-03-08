9605ebdacb273e4910dba1593339801023f8890b
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-03-08 17:29:30 -0500

    Server starting code, add validation error

--- server/models/users.js
+++ server/models/users.js
@@ -2,7 +2,7 @@ var mongoose = require( 'mongoose' );
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 
-var config = respond('../../config');
+var config = require('../../config');
 
 var userSchema = new mongoose.Schema({
   email: {
