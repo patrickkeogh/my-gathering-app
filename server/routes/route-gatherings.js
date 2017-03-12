c6f7cd38def38c16a9311b8d2596c5154af92431
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-03-12 15:47:04 -0400

    Update get gathering filter and query

--- server/routes/route-gatherings.js
+++ server/routes/route-gatherings.js
@@ -43,7 +43,7 @@ gatheringRouter.route('/')
 
     // });
 
-    Gatherings.find({}, function (err, gatherings) {
+    Gatherings.find(query, function (err, gatherings) {
         if (err) throw err;
         res.json(gatherings);
     });
