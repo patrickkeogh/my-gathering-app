575ef928cdbd1e03540d2d6e58c3c390c6aaf7c4
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-03-13 20:43:21 -0400

    Add banner upload and crop

--- client/scripts/controllers/gathering.info.ctr.js
+++ client/scripts/controllers/gathering.info.ctr.js
@@ -10,8 +10,26 @@
       var vm = this;
 
       vm.selectedGatheringId = $stateParams.id;
+
+      vm.gathering = {};
+
+      angular.element(document).ready(function() {
+
+        console.log("init called in Gathering Info");
+
+        gatheringAPI.getGathering(vm.selectedGatheringId)
+        .then(function(data) {
+          console.log(data);
+          vm.gathering = data.data;
+        })
+        .catch(function(err) {
+          console.log('failed to get gathering ' + err);
+        });
+
+       
+      });
       
-      vm.message = "Hello";
+
       
     }
 }());
