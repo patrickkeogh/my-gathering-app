2a9f4580f74279746b3c9d173e46c12f9e2fcabb
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-04-30 19:20:13 -0400

    Add email notification on registration

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var app = express();
 // });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 //app.set('view engine', 'hbs');
 app.set('view engine', 'jade');
 
