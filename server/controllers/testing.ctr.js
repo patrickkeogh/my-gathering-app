3c3f0a9b055ca5ee09fc1de705bc81172d8cb6ba
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-05-19 09:27:32 -0400

    New route for testing

---
+++ server/controllers/testing.ctr.js
@@ -0,0 +1,33 @@
+var HTTPStatus = require('http-status');
+
+// Bring in the required models
+var Gatherings = require('../models/gatherings');
+var Topic = require('../models/topics');
+var Type = require('../models/types');
+
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+var validationError = function(res, err) {
+  return res.json(422, err);
+};
+
+module.exports.getGatheringPojo = function(req, res) {
+  console.log('entered getGatheringPojo:');
+
+  Gatherings.find(queryObj, {_id:1, name: 1}).exec(function(err, gatherings) {
+
+      if (err) throw err;
+      sendJSONresponse(res, HTTPStatus.OK, gatherings);
+
+  });
+
+
+
+};
+
+
+
